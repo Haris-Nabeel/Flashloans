8854cd5787c0f6fcb016f0e884f0f167fa1f70a5
HarisNabeel <harisnabeel98@gmail.com>
2025-01-15 13:49:09 +0500

    Added ethwhale for fork test

--- test/MorphoFlashloan.t.sol
+++ test/MorphoFlashloan.t.sol
@@ -11,12 +11,14 @@ contract MorphoFlashloanTest is Test {
     address public loanToken = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913; // USDC on base
     uint public loanAmount = (1 * 10 ** 6) * 10 ** 6;
     bytes public callbackData = abi.encode(loanToken, 99);
+    address public ethWhale = 0xF977814e90dA44bFA03b6295A0616a897441aceC; // ethWhale on base
     function setUp() public {
         morphoFlashloan = new MorphoFlashloan(morpho);
     }
 
     function test_flashloan() public {
         assertEq(morphoFlashloan.luckyNumber(), 0);
+        vm.prank(ethWhale);
         morphoFlashloan.takeFlashLoan(loanToken, loanAmount, callbackData);
         assertEq(morphoFlashloan.luckyNumber(), 99);
     }
