83885230452ddfe727f33b80f48bf24541137293
HarisNabeel <harisnabeel98@gmail.com>
2025-01-15 12:54:13 +0500

    first commit

---
+++ test/MorphoFlashloan.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MorphoFlashloan} from "../src/MorphoFlashloan.sol";
+
+contract MorphoFlashloanTest is Test {
+    function setUp() public {}
+}
